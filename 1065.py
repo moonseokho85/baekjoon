0def501081a0539882814bfb77e4d3373041f886
Logan Moon <mseokho7733@gmail.com>
2021-03-20 17:30:34 +0900

    add remark in 1065.py

--- 1065.py
+++ 1065.py
@@ -1,14 +1,19 @@
+# Function of hansu
 def hansu(n):
     counts = 0
     for j in range(1, int(n) + 1):
 
+        # Stringify input number
         j = str(j)
+
+        # If input number is one digit, count it
         if len(j) == 1:
             counts += 1
 
         subtracts = list()
         for i in range(len(j) - 1):
 
+            # Judge the isometric sequence
             subtract = int(j[i + 1]) - int(j[i])
             subtracts.append(subtract)
 
@@ -18,5 +23,8 @@ def hansu(n):
     print(counts)
 
 
+# Input number
 N = input()
+
+# Calculate of input number through function of hansu
 hansu(N)
